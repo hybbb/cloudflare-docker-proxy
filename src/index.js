2e819593e70618b527e7247403e16c1a70ee0ea3
hybbb <hybbb@foxmail.com>
2024-06-26 10:58:43 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.dadaohyb.eu.org": dockerHub,
+  "quay.dadaohyb.eu.org": "https://quay.io",
+  "gcr.dadaohyb.eu.org": "https://gcr.io",
+  "k8s-gcr.dadaohyb.eu.org": "https://k8s.gcr.io",
+  "k8s.dadaohyb.eu.org": "https://registry.k8s.io",
+  "ghcr.dadaohyb.eu.org": "https://ghcr.io",
+  "cloudsmith.dadaohyb.eu.org": "https://docker.cloudsmith.io",
+  "ecr.dadaohyb.eu.org": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.dadaohyb.eu.org": dockerHub,
 };
 
 function routeByHosts(host) {
